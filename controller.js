631e69d343faca8807e7521da7ab3d9a7e8c4ed5
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-14 09:02:36 -0500

    add items working

--- controller.js
+++ controller.js
@@ -3,9 +3,14 @@
 	function FormController() {
 		var vm = this;
 		vm.toDoItem = [];
-		vm.updateInfo = function() {
-			console.log("HELLO WORLD");
+		vm.updateInfo = function(item) {
+			vm.toDoItem.push(item);
+			document.getElementById("inputEl").value = "";
+			console.log(item);
 		};
+		vm.remove = function(){
+			vm.toDoItem.splice()
+		}
 	}
 
 	angular
